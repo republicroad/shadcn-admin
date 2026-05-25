09067b4fa06e2d060d85255cc672dc949c30c8ee
RYefccd <847960106@qq.com>
2026-05-25 15:18:56 +0800

    refactor: refact auth feature code structures

--- src/features/auth/services/mocks.ts
+++ src/features/auth/services/mocks.ts
@@ -1,13 +1,14 @@
-// src/mocks/handlers.ts
+// 如何对针对某个单独的接口开启和关闭 msw mock 功能?
 import { http, HttpResponse } from 'msw'
+import { type User } from '../types/auth.types'
 import { authService, authEndpoint } from './auth'
-import { fake_user_login, type LoginUser } from './utils'
+import { fake_user_login } from './utils'
 
 // todo: 考虑如何比较优雅的开启和关闭 mock 某个接口的数据
 export const handlers = [
   // mock数据, 类型和逻辑都可以封装到一个单独的模块里, 以保持 handlers.ts 的简洁和可维护性.
   http.post(authEndpoint.login, async ({ request }) => {
-    const requestBody = (await request.json()) as LoginUser
+    const requestBody = (await request.json()) as User
     return HttpResponse.json(await fake_user_login(requestBody))
   }),
   http.post(authEndpoint.register, async ({ request }) => {
--- src/features/auth/types/auth.types.tsx
+++ src/features/auth/types/auth.types.tsx
@@ -1,4 +1,4 @@
-export interface credentials {
+export interface User {
   email: string
   password: string
 }
